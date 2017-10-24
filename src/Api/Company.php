064a06811e0d38b46b36e5b80ad18a7260530dba
Jeffrey Hyer <jmhyer135@gmail.com>
2017-10-24 14:10:46 -0600

    Documented the Company API

--- src/Api/Company.php
+++ src/Api/Company.php
@@ -5,11 +5,23 @@ namespace BambooHR\Api;
 class Company extends AbstractApi
 {
 
+    /**
+     * Get a list of files and categories associated with the company
+     *
+     * @return BambooHR\Api\Response
+     */
     public function files()
     {
         return $this->get("files/view");
     }
 
+    /**
+     * Add a file category
+     *
+     * @param string $category
+     * 
+     * @return BambooHR\Api\Response
+     */
     public function addFileCategory($category)
     {
         $xml = "<files><category>{$category}</category></files>";
@@ -17,6 +29,14 @@ class Company extends AbstractApi
         return $this->post("files/categories", $xml);
     }
 
+    /**
+     * Update a given file
+     *
+     * @param string $fileId
+     * @param array $data ['name' => string, 'categoryId' => integer, 'shareWithEmployee' => 'yes|no']
+     * 
+     * @return BambooHR\Api\Response
+     */
     public function updateFile($fileId, array $data)
     {
         $xml = "<file>";
@@ -38,16 +58,37 @@ class Company extends AbstractApi
         return $this->post("files/{$fileId}", $xml);
     }
 
+    /**
+     * Delete a given file
+     *
+     * @param string $fileId
+     * 
+     * @return BambooHR\Api\Response
+     */
     public function deleteFile($fileId)
     {
         return $this->delete("files/{$fileId}");
     }
 
+    /**
+     * Download a given file
+     *
+     * @param string $fileId
+     * 
+     * @return BambooHR\Api\Response
+     */
     public function downloadFile($fileId)
     {
         return $this->get("files/{$fileId}");
     }
 
+    /**
+     * Upload a new file
+     *
+     * @param array $data
+     * 
+     * @return BambooHR\Api\Response
+     */
     public function uploadFile(array $data)
     {
         return $this->postFile("files", $data);
